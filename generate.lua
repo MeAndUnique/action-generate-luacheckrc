a4a131dbf12abf55adcf3e85752dc42a438790c6
Wil Thieme <wil.thieme@protonmail.com>
2022-04-13 22:29:44 -0400

    initial implementation
    requires rework

--- generate.lua
+++ generate.lua
@@ -1,16 +1,25 @@
 -- Config
 local dataPath = './.fg/'
 
-local stdBase = 'lua51+fg+fgfunctions'
+local stdBase = '[selene.base]\nbase = "lua51"\nname = "fantasygrounds"'
 local stdString = arg[1] or ''
-local headerFileName = arg[2] or '.luacheckrc_header'
-local outputFile = arg[3] or '.luacheckrc'
+local headerFileName = arg[2] or 'fglualibrary_header.toml'
+local outputFile = arg[3] or 'fglualibrary.toml'
+
+-- Datatypes
+local packageTypes = {
+	['rulesets'] = { dataPath .. 'rulesetsglobals/', 'base.xml' },
+	['extensions'] = { dataPath .. 'extensionsglobals/', 'extension.xml' },
+}
 
 -- Core
 local lfs = require('lfs')
 
 -- open new luachecrc file for writing and post error if not possible
 local destFile = assert(io.open(outputFile, 'w'), 'Error opening file ' .. outputFile)
+
+-- add selene meta info to luachecrc file
+destFile:write(stdBase .. '\n' .. stdString .. '\n\n')
 
 -- open header file and add to top of new config file
 local headerFile = io.open(headerFileName, 'r')
@@ -25,32 +34,25 @@ local function findPackageFiles(path)
 
 	for file in lfs.dir(path) do
 		local fileType = lfs.attributes(path .. '/' .. file, 'mode')
-		local packageName = string.match(file, '(.*).luacheckrc_std')
-		if packageName and fileType == 'file' then
-			if file ~= '.' and file ~= '..' then result[packageName] = path .. '/' .. file end
-		end
+		local packageName = string.match(file, '(.*)globals.lua')
+		if packageName and fileType == 'file' then if file ~= '.' and file ~= '..' then result[packageName] = path .. '/' .. file end end
 	end
 
 	return result
 end
 
-local packageFiles = findPackageFiles(dataPath .. 'globals/')
-
--- add std config to luachecrc file
-destFile:write("\nstd = '" .. stdBase)
-for packageName, _ in pairs(packageFiles) do destFile:write('+' .. packageName) end
-destFile:write(stdString)
-destFile:write("'\n")
-
 -- looks through each package type's detected globals
 -- it then appends them to the config file
-for packageName, file in pairs(packageFiles) do
-	local stdsName = ('\nstds.' .. packageName .. ' = {\n')
-	destFile:write(stdsName)
-	local fhandle = io.open(file, 'r')
-	local content = fhandle:read('*a')
-	for line in string.gmatch(content, '[^\r\n]+') do destFile:write('\t' .. line .. '\n') end
-	destFile:write('}\n')
+for _, packageType in pairs(packageTypes) do
+	local packageFiles = findPackageFiles(packageType[1])
+	for packageName, file in pairs(packageFiles) do
+		local stdsName = ('# ' .. packageName:lower() .. ' definitions\n')
+		destFile:write(stdsName)
+		local fhandle = io.open(file, 'r')
+		local content = fhandle:read('*a')
+		for line in string.gmatch(content, '[^\r\n]+') do destFile:write(line .. '\n') end
+		destFile:write('\n')
+	end
 end
 
 destFile:close()
